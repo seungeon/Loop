47428f2a613cd38136f0cc16083da6758222f7a3
jungbiho <wjdqlgh@naver.com>
2014-09-11 16:07:01 +0900

    sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
 function setup() {
   createCanvas(640, 480);
-  background(128, 0, 64);
+ /* background(128, 0, 64); */
   noFill();
 
   stroke(255, 0, 0);
+}
+
 /*
   var x;
 //
@@ -28,8 +30,16 @@ for (var x = 1; x <= 8; x++)
 function draw() {
   background(128, 0, 64);
 
-  ellipse(320, 240, mouseX, mouseY);
+for 
+  (var x = 1; x <= 8; x++)
+{
+
+  ellipse(x * 70, x * 20 + 100, x+50, x*10);
+
 }
+var size=Math.abs(mouseX - 320);
+  ellipse(320, 240, mouseX, mouseY);
+
 
 }
 
