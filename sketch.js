20b02ce3c0fd4f9359088d2e0b12a3ae1307e12b
jungbiho <wjdqlgh@naver.com>
2014-09-11 15:37:26 +0900

    sketch.js

--- sketch.js
+++ sketch.js
@@ -3,9 +3,7 @@ function setup() {
   background(128, 0, 64);
   noFill();
 
-
   stroke(255, 0, 0);
-
 /*
   var x;
 //
@@ -21,11 +19,9 @@ function setup() {
 */
 
 
-
-
-for (var x = 1; x <=3; x++)
+for (var x = 1; x <= 8; x++)
 {
-  ellipse(x * 100, 100, 50, 50);
+  ellipse(x * 70, x * 20 + 100, 50, 50);
 }
 
 }
