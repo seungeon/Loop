83e9dc20cfd688df4e4ebcba577c9bcd13923b06
seungeon <seungeon.k@gmail.com>
2014-10-01 15:17:03 +0900

    index3.html

---
+++ sketch45.js
@@ -0,0 +1,33 @@
+	function draw() {
+
+ 
+  background(240,240,255);
+  
+
+	//red
+	  fill(220,20,60);
+	  stroke(220,20,60);
+	  rect(mouseX+20,mouseY+20,10,10);
+	  rect(mouseX+20,mouseY+30,30,10);
+	  rect(mouseX+20,mouseY+40,40,10);
+//white
+	  fill(255,248,220);
+	  stroke(255,248,220);
+	  rect(mouseX+20,mouseY+50,50,40);
+	  rect(mouseX+20,mouseY+90,70,40);
+	  //yello;
+	  	  fill(255,127,80);
+	  stroke(255,127,80);
+	  rect(mouseX+10,mouseY+70,10,20);
+	  //black
+
+	  fill(0);
+	  stroke(0);
+	  rect(mouseX+30,mouseY+60,10,10)
+	    //brown;
+	  fill(165,42,42);
+	  stroke(165,42,42);
+	  rect(mouseX+40,mouseY+130,10,30);
+	  rect(mouseX+30,mouseY+150,10,10);
+}
+
