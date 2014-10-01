c50495d5542ba69595f093486401737712d20c81
seungeon <seungeon.k@gmail.com>
2014-10-01 15:31:01 +0900

    sketch45.js

--- sketch45.js
+++ sketch45.js
@@ -1,7 +1,7 @@
 	function draw() {
 
- 
-  background(240,240,255);
+
+  background(255,20,147);
   
 
 	//red
@@ -29,5 +29,30 @@
 	  stroke(165,42,42);
 	  rect(mouseX+40,mouseY+130,10,30);
 	  rect(mouseX+30,mouseY+150,10,10);
+
+
+	  //병아리
+
+	  	  fill(255,248,220);
+	  stroke(255,248,220);
+	  rect(mouseX+120,mouseY+90,40,50);
+
+	  	  fill(0);
+	  stroke(0);
+	  rect(mouseX+130,mouseY+100,10,10);
+
+	  	  	  fill(255,127,80);
+	  stroke(255,127,80);
+	  rect(mouseX+110,mouseY+110,10,10);
+
+	    fill(165,42,42);
+	  stroke(165,42,42);
+	  rect(mouseX+140,mouseY+140,10,20);	  
+	  rect(mouseX+130,mouseY+150,10,10);
+
+
 }
 
+function setup() {
+  createCanvas(500, 410);
+  }
